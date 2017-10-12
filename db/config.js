a887860d72180364d6030bde65a8f5f829c0c454
Priya Jain <pjain0371@gmail.com>
2017-10-12 21:47:15 +0530

    Code

--- db/config.js
+++ db/config.js
@@ -1,5 +1,6 @@
 var dbConfig = {
-    dbURL: "mongodb://localhost:27017/onlineshop"
+    /*dbURL: "mongodb://localhost:27017/onlineshop"*/
+    dbURL: "mongodb://admin:admin@ds115625.mlab.com:15625/users"
 }
 
 module.exports = dbConfig;
